5ccc07ae4f9e1aa66884f0f23b59fd7237954e88
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-14 13:59:00 +0700

    fixing seeder file

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,4 +1,4 @@
 pages << {
-	url: 'https://www.aliexpress.com/category/100003109/women-clothing.html'
+	url: 'https://www.aliexpress.com/category/100003109/women-clothing.html',
 	page_type: 'listings'
 }
