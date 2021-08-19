a87347a30cd7c00140a5891fe5a68adc48a60b3b
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-20 00:18:11 +0700

    fixed parser for products

--- parsers/products.rb
+++ parsers/products.rb
@@ -10,11 +10,11 @@ product['image_url'] = nokogiri.at_css('.image-view-magnifier-wrap img')['src']
 price_container = nokogiri.css('.product-price')
 if price_container
 	product['current_price'] = price_container.at_css('.product-price-current span').text
-	product['discount_elemen'] = price_container.at_css('.product-price-original span').text
-	if product['discount_elemen']
+	product['discount'] = price_container.at_css('span.product-price-mark').text
+	if product['discount']
 		product['original_price'] = price_container.at_css('.product-price-original .product-price-del span').text
-	else
-		product['original_price'] = product['current_price']
+#	else
+#		product['original_price'] = product['current_price']
 	end
 end
 
@@ -24,19 +24,19 @@ if skus_element
 	product['skus'] = skus
 end
 
-sizes_element = nokogiri.css('sku-property-list')
+sizes_element = nokogiri.css('ul.sku-property-list')
 if sizes_element
-	sizes = sizes_element.at_css('li div span').text
+	sizes = sizes_element.css('div.sku-property-text').collect {|i| i.text.strip}
 	product['sizes'] = sizes
 end
 
-product['rating'] = nokogiri.at_css('div.positive-fdbk')[1].text
+product['rating'] = nokogiri.css('div.positive-fdbk')[0].text
 
 product['orders_count'] = nokogiri.at_css('span.product-reviewer-sold').text.strip.split(' ').first.to_i
 
-return_element = nokogiri.css('div.buyer-pretection-content')
+return_element = nokogiri.css('div.buyer-pretection-context')
 if return_element
-	product['return_policy'] = return_policy.at_css('div.buyer-pretection-context').text
+	product['return_policy'] = return_element.text
 end
 
 product['_collection'] = 'products'
