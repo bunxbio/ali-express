7fbeb97e48105813cfa7cb095d3689b326af9ea5
bunxbio <biokenfigumilang@gmail.com>
2021-08-31 01:07:39 +0700

    adding pagination in listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,18 +1,21 @@
+driver_code = "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
+
 nokogiri = Nokogiri.HTML(content)
 
 products = nokogiri.css('.JIIxO ._1OUGS')
-product_ids = []
 
 products.each do |product|
 	a_element = product.at_css('.awV9E')
-	url = URI.join('https://www.aliexpress.com', a_element['href']).to_s.split('?').first
-
-	product_ids << a_element['href'].scan(/[0-9]+/)[0]
+	url = "https://www.aliexpress.com#{a_element['href'].to_s.split('?').first}"
 
 	pages << {
 		url: url,
 		page_type: 'products',
 		fetch_type: 'browser',
+		method: 'GET',
 		force_fetch: true,
 		vars: {
 			page_num: page['vars']['page_num'],
@@ -22,14 +25,26 @@ products.each do |product|
 	}
 end
 
-url = "https://www.aliexpress.com/glosearch/api/product?trafficChannel=main&catName=women-clothing&CatId=100003109&ltype=wholesale&SortType=default&page=2&origin=y&pv_feature=#{product_ids.join(',')}"
-pages << {
-	url: url,
-	page_type: 'listings_json',
-	force_fetch: true,
-	method: 'GET',
-	headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-	vars: {
-		page_num: 2
+
+current_page_num = page['vars']['page_num']
+
+if products
+	next_page = "https://www.aliexpress.com/category/100003109/women-clothing.html?trafficChannel=main&catName=women-clothing&CatId=100003109&ltype=wholesale&SortType=default&page=#{current_page_num + 1}"
+
+	pages << {
+		url: next_page,
+		page_type: 'listings',
+		fetch_type: 'browser',
+		method: 'GET',
+		force_fetch: true,
+		vars: {
+			page_num: current_page_num+1
+		},
+		headers: {
+			"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+		},
+		driver: {
+			code: driver_code
+		}
 	}
-}
+end
